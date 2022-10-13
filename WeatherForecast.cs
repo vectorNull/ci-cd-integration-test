572456c8bba347affbd5a09bd30e8a91c77c85ab
Jose (Joseph) Rodriguez <jose.rodriguez@prominentcode.com>
2022-10-13 08:01:38 -0700

    add code

---
+++ WeatherForecast.cs
@@ -0,0 +1,12 @@
+namespace ci_cd_int_test;
+
+public class WeatherForecast
+{
+    public DateTime Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+}
